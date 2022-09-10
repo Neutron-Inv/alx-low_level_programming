3bce6f80127e3012fc296e3ea8eed77b9de2e2da
Emmanuel <jackomega.idnoble@gmail.com>
2022-09-09 20:39:45 -0700

    new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,73 @@
-#include <stdio.h>
 
-/**
- *main - print all combinations of two units i.e 00-99
- *
- *return: Always 0 (Success)
+
+/*
+
+ * File: 100-print_comb3.c
+
+ * Auth: Brennan D Baraban
+
  */
 
- int main(void)
- {
-   int unit;
-   int tenth;
 
-   for (tenth = '0'; tenth <= 9; tenth++)
-     {
-       for (unit =(tenth + 1); unit <= 9; unit++)
-	 {
-	   putchar(tenth);
-	   putchar(unit);
 
-	   if (tenth != '8' || unit != '9')
-	     {
-	       putchar(',');
-	       putchar(' ');
-	     }
-	 }
-     }
-   putchar('\n');
+#include <stdio.h>
 
-   return (0);
- }
+
+
+/**
+
+ * main - Prints all possible combinations of two different digits,
+
+ *        in ascending order, separated by a comma followed by a space.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
+{
+  
+  int first, second;
+  
+
+  
+  for (first = 0; first < 9; first++)
+    
+    {
+      
+      for (second = first + 1; second < 10; second++)
+	
+	{
+	  
+	  putchar((first % 10) + '0');
+	  
+	  putchar((second % 10) + '0');
+	  
+
+	  
+	  if (first == 8 && second == 9)
+	    
+	    continue;
+	  
+
+	  
+	  putchar(',');
+	  
+	  putchar(' ');
+	  
+	}
+      
+    }
+  
+
+  
+  putchar('\n');
+  
+
+  
+  return (0);
+  
+}
