dc4eca75b84e2f0edcf6d86958f67c84d52fc7c6
Emmanuel <jackomega.idnoble@gmail.com>
2022-09-09 21:09:38 -0700

    new commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,16 @@
  * Return: Always 0.
  */
 
-int main(void) {
+int main(void)
+{
 
 int num;
 
-for (num = 0; num <= 9; num++) {
+for (num = 0; num <= 9; num++)
+{
 putchar((num % 10) + '0');
 
-if (num == 9)      
+if (num == 9)
 continue;
 
 putchar(',');
@@ -21,9 +23,9 @@ putchar(',');
 putchar(' ');
 
 }
-  
+
 putchar('\n');
 
 return (0);
-  
+
 }
