5626a23cbc44eaa0ca57ca648a2bf25254117c9f
Emmanuel <jackomega.idnoble@gmail.com>
2022-09-09 21:04:22 -0700

    new commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,44 +1,33 @@
 #include <stdio.h>
 
-
-
 /**
-
- *main - print 0-9 separated with commas, using putchar
-
- *Return: Always 0 (Success)
-
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
  */
 
-
-
-int main(void)
+int main(void) {
   
-{
-  
-  int n = '0';
-  
+  int num;
 
   
-  while (n <= '9')
+  for (num = 0; num <= 9; num++) {
     
-    {
+    putchar((num % 10) + '0');
+    
+    if (num == 9)
       
-      putchar(n);
-      
-      if (n != '9')
-	
-	{
-	  
-	  putchar(',');
-	  
-	  putchar(' ');
-	  
-	}
-      
-      n++;
-      
-    }
+      continue;
+    
+
+    
+    putchar(',');
+    
+    putchar(' ');
+    
+  }
+  
+
   
   putchar('\n');
   
