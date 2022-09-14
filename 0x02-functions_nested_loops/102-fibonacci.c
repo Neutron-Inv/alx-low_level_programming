4155667310f8ef60b26171edf98aea2288eb2a32
Emmanuel <jackomega.idnoble@gmail.com>
2022-09-13 17:19:14 -0700

    new commut

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,13 +15,12 @@ long a = 1;
 long b = 2;
 for (counter = 1; counter <= (countto / 2); counter++)
 {
-printf("%li %li ", a, b);
+printf("%li, %li, ", a, b);
 a += b;
 b += a;
 }
 if (countto % 2 == 1)
 printf("%li", a);
-printf(",");
 printf("\n");
 return (0);
 }
