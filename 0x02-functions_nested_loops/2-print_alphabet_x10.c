2085a350a78c6db4cb6dfd71e7562fd7f304a438
Emmanuel <jackomega.idnoble@gmail.com>
2022-09-13 17:07:36 -0700

    new commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,17 @@
  */
 
 void print_alphabet_x10(void)
- 
-{  
+
+{
 char alpha;
 int counter;
 for (counter = 1; counter <= 10; counter++)
 {
-for (alpha = 'a'; alpha <= 'z'; alpha++)	
+for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 _putchar(alpha);
-}      
+}
 _putchar('\n');
 
-} 
+}
 }
