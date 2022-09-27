fbca899b2f894145d55fb0cc90a34c42d9b96f7b
Neutron-Inv <102728790+Neutron-Inv@users.noreply.github.com>
2022-09-27 01:21:48 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,33 +1,33 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strpbrk - Search a string for any of a set of bytes.
- * @s: string
- * @accept: string to match
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
- */
-
+  * _strpbrk - search a string for any of a set of bytes
+  * @s: source string
+  * @accept: accepted characters
+  *
+  * Return: the string since the first found accepted character
+  */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
-char *p;
+int a = 0, b;
 
-i = 0;
-while (s[i] != '\0')
+while (s[a])
 {
-j = 0;
-while (accept[j] != '\0')
+b = 0;
+
+while (accept[b])
 {
-if (accept[j] == s[i])
+if (s[a] == accept[b])
 {
-p = &s[i];
-return (p);
-}
-j++;
-}
-i++;
+s += a;
+return (s);
 }
 
-return (0);
+b++;
+}
+
+a++;
+}
+
+return ('\0');
 }
