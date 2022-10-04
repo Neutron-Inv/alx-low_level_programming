b04ab8b322dd53c888c0307a28173e165a37e923
Neutron-Inv <102728790+Neutron-Inv@users.noreply.github.com>
2022-10-04 23:38:18 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints program name, followed by a new line
+ * main - prints the number of arguments passed into the program
  * @argc: int
  * @argv: list
  * Return: 0
  */
 
-
 int main(int argc, char const *argv[])
 {
 (void)argv;
-printf("%i\n", argc - 1)
-return(0);
+printf("%i\n", argc - 1);
+return (0);
 }
