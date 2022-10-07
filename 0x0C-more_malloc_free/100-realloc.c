97987d8e96cef69e419c9949cf035d8447f90e81
Neutron-Inv <102728790+Neutron-Inv@users.noreply.github.com>
2022-10-07 03:45:53 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,51 +1,137 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+/**
+  * int_calloc - special calloc but 4 int arrays
+  * @nmemb: n memb
+  * @size: size of array
+  * Return: int *
+  */
+int *int_calloc(int nmemb, unsigned int size)
+{
+/* declarations */
+int *p, n;
+/* checking inputs */
+if (nmemb == 0 || size == 0)
+return (NULL);
+/* malloc the space & check for fail */
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+/* calloc */
+for (n = 0; n < nmemb; n++)
+p[n] = 0;
+return (p);
+}
 
 /**
-  * _realloc - ...
-  * @ptr: ...
-  * @old_size: ...
-  * @new_size: ...
-  *
-  * Return: ...
+  * mult - multiplication
+  * @product: int * 4 answer
+  * @n1: string num1
+  * @n2: string num2
+  * @len1: len num1
+  * @len2: len num2
+  * Return: void
   */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void mult(int *product, char *n1, char *n2, int len1, int len2)
 {
-char *nptr;
-unsigned int i;
-
-if (new_size == old_size)
-return (ptr);
-
-if (ptr == NULL)
+	/* declarations */
+int i;
+int j;
+int f1, f2;
+int sum;
+	/* the long math */
+for (i = len1 - 1; i >= 0; i--)
 {
-nptr = malloc(new_size);
-
-if (nptr == NULL)
-return (NULL);
-
-return (nptr);
+sum = 0;
+f1 = n1[i] - '0';
+for (j = len2 - 1; j >= 0; j--)
+{
+f2 = n2[j] - '0';
+sum += product[i + j + 1] + (f1 * f2);
+product[i + j + 1] = sum % 10;
+sum /= 10;
 }
-else
-{
-if (new_size == 0)
-{
-free(ptr);
-return (NULL);
+if (sum > 0)
+product[i + j + 1] += sum;
 }
-}
-
-nptr = malloc(new_size);
-
-if (nptr == NULL)
-return (NULL);
-
-for (i = 0; i < old_size && i < new_size; i++)
-{
-nptr[i] = ((char *) ptr)[i];
+for (i = 0; product[i] == 0 && i < len1 + len2; i++)
+{}
+if (i == len1 + len2)
+_putchar('0');
+for (; i < len1 + len2; i++)
+_putchar(product[i] + '0');
+_putchar('\n');
 }
 
-free(ptr);
-return (nptr);
+/**
+  * is_valid - is the number a valid one
+  * @num : char string num
+  * Return: int, 1 if true 0 if false
+  */
+int is_valid(char *num)
+{
+/* declarations */
+int i;
+/* checking for ints */
+for (i = 0; num[i]; i++)
+{
+if (num[i] < '0' || num[i] > '9')
+return (0);
+}
+return (1);
+}
+/**
+  * err - errors r us
+  * @status: error code 4 exit
+  * Return: void
+  */
+void err(int status)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+exit(status);
+}
+/**
+  * main - getting the args
+  * @argc: args #
+  * @argv: arg array
+  * Return: 0
+  */
+int main(int argc, char **argv)
+{
+/* declarations */
+int i, j, len1 = 0, len2 = 0;
+int *res;
+/* too many args? too few? */
+if (argc != 3)
+{
+err(98);
+}
+/* using isvalid */
+for (i = 1; i < argc; i++)
+{
+if (!(is_valid(argv[i])))
+err(98);
+if (i == 1)
+{
+for (j = 0; argv[i][j]; j++)
+len1++;
+}
+if (i == 2)
+{
+for (j = 0; argv[i][j]; j++)
+len2++;
+}
+}
+res = int_calloc(len1 + len2, sizeof(int));
+if (res == NULL)
+err(98);
+mult(res, argv[1], argv[2], len1, len2);
+free(res);
+return (0);
 }
